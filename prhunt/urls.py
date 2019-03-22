8f1f58fc5d6667e6601f3da8f679a9135e61a07f
Mahdi <mkaramidarabi@gmail.com>
2019-03-22 23:06:43 +0430

    added static directory and url in project

--- prhunt/urls.py
+++ prhunt/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views as pviews
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', pviews.welcome, name='welcome'),
     path('pr/', include('products.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
